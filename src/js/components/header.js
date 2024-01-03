76653c356e1e60d3c301565540ea6fb3d3af25a0
ticial <49561842+ticial@users.noreply.github.com>
2024-01-03 17:02:52 +0200

    fied after review

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,41 +1,42 @@
 const header = () => {
 	const mainMenu = document.querySelector('.main-menu');
-	const menuOpenButton = document.querySelector('.menu-button_open');
-	const menuCloseButton = document.querySelector('.menu-button_close');
+	const menuToggleButton = document.querySelector('.menu-button');
 
 	let openCloseTimeout;
 
-	function openMenu(event) {
-		event.stopPropagation();
-		if (!mainMenu.classList.contains('main-menu_open')) {
+	function menuToggle(setOpen) {
+		if (!setOpen || menuToggleButton.classList.contains('menu-button_close')) {
+			menuToggleButton.textContent = 'menu';
+			menuToggleButton.classList.remove('menu-button_close');
+			menuToggleButton.classList.add('menu-button_open');
+
+			clearTimeout(openCloseTimeout);
+			mainMenu.classList.remove('main-menu_open');
+			openCloseTimeout = setTimeout(() => mainMenu.classList.add('main-menu_close'), 1000);
+		} else {
+			menuToggleButton.textContent = 'close';
+			menuToggleButton.classList.remove('menu-button_open');
+			menuToggleButton.classList.add('menu-button_close');
+
 			clearTimeout(openCloseTimeout);
 			mainMenu.classList.remove('main-menu_close');
 			openCloseTimeout = setTimeout(() => mainMenu.classList.add('main-menu_open'), 0);
 		}
 	}
 
-	function closeMenu() {
-		if (mainMenu.classList.contains('main-menu_open')) {
-			clearTimeout(openCloseTimeout);
-			mainMenu.classList.remove('main-menu_open');
-			openCloseTimeout = setTimeout(() => mainMenu.classList.add('main-menu_close'), 1000);
-		}
-	}
-
 	document.addEventListener('keydown', (event) => {
 		if (event.code === 'Escape') {
-			closeMenu();
+			menuToggle(false);
 		}
 	});
 
 	document.addEventListener('click', (event) => {
-		if (!mainMenu.contains(event.target)) {
-			closeMenu();
+		if (!mainMenu.contains(event.target) && !menuToggleButton.contains(event.target)) {
+			menuToggle(false);
 		}
 	});
 
-	menuOpenButton.addEventListener('click', openMenu);
-	menuCloseButton.addEventListener('click', closeMenu);
+	menuToggleButton.addEventListener('click', menuToggle);
 };
 
 export default header;
